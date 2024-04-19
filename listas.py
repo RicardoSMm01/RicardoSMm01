6af94c39b564030f4ada190e4bf10a1f3c049c16
RicardoSMm01 <sant.ricard.2401@gmail.com>
2024-04-18 19:17:37 -0600

    first commit

---
+++ listas.py
@@ -0,0 +1,15 @@
+print("Lista vacia")
+
+lista_vacia = []
+print(lista_vacia)
+
+print("\nListas")
+datos = ["Carlos", 20, 1.7, True]
+print(datos)
+
+print("Accediendo a las pociciones de una lista")
+marcas = ["Apple", "Samsung", "Xiaomi", "Huawei"]
+
+print(f"\nLista completa marcas: {marcas}")
+print(f"\n¿Cuantos elementos tiene la lista? - len(marcas): {len(marcas)}")
+print(f"\nmarcas[1]: {marcas[1]} ")
